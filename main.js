ec9231b68cf6b81da9955cc10c8bae844c94c2d4
Jully Achenchi <jullyachenchi8@gmail.com>
2024-10-18 17:01:29 +0300

    Website updated

--- main.js
+++ main.js
@@ -141,3 +141,21 @@ const sr = ScrollReveal({
 sr.reveal('.home__perfil, .about__image, .contact__mail', {origin: 'right'})
 sr.reveal('.home__name, .home__info, .about__container .section__title-1, .about__info, .contact__social, .contact__data', {origin: 'left'})
 sr.reveal('.services__card, .projects__card', {interval: 100})
+
+/*=============== VISITOR COUNTER ====================*/
+const apiUrl = ''
+async function updateVisitorCount() {
+    try{
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        const visitorCount = data.visitorCount;
+
+        document.getElementById('visitor-number').textContent = 'Visitors: ${visitorCount}';
+
+    } catch(error) {
+        console.error('Error fetching visitor count:', error);
+        document.getElementById('visitor-number').textContent = 'Error loading count';
+    }
+    
+}
+updateVisitorCount();
